c68bbe3736a8181b518855f9ae4e220ccdb92703
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-07-24 15:33:53 +0900

    Feat: 프로필페이지, 프로필페이지-스타일 업데이트 스타일부분은 components폴더에 profile 폴더 만든후 안에 ProfilePageStyle.jsx로 만들어서 임포트해서 사용

---
+++ src/components/profile/ProfilePageStyle.jsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+`;
+
+export const ProfileHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding-bottom: 31px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const ProfileImage = styled.div`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #ddd;
+  margin-right: 20px;
+`;
+
+export const ProfileName = styled.h3`
+  font-size: 16px;
+  font-weight: 400;
+  color: #333333;
+`;
+
+export const ProfileSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+`;
+
+export const ProfileLabel = styled.button`
+  background: none;
+  border: none;
+  font-size: 14px;
+  color: #333333;
+  cursor: pointer;
+`;
+
+export const ProfileEdit = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    transform: rotate(180deg);
+  }
+`;
+
+export const ProfileDelete = styled.button`
+  background: none;
+  border: none;
+  font-size: 14px;
+  color: #333333;
+  cursor: pointer;
+`;
+
+export const ProfileLogout = styled.button`
+  width: 100%;
+  height: 56px;
+  font-size: 14px;
+  border: 1px solid #333333;
+  border-radius: 10px;
+  background: none;
+  margin-top: auto;
+  cursor: pointer;
+`;
