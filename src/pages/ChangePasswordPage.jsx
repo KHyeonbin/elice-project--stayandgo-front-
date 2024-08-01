d939685f0a5b50253770ef66f8d0648dd06e614f
boeun <supbe0726@naver.com>
2024-08-01 18:39:16 +0900

    Feat, DesignCSS: css수정, 페이지 이동 애니메이션 추가

--- src/pages/ChangePasswordPage.jsx
+++ src/pages/ChangePasswordPage.jsx
@@ -1,13 +1,18 @@
 import SubLayout from "../components/layout/SubLayout";
 import ChangePassword from "../components/account/ChangePassword";
+import { motion } from "framer-motion";
 
 const ChangePasswordPage = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0, transform: 'translateX(100%)' }}
+      animate={{ opacity: 1, transform: 'translateX(0)' }}
+      transition={{ duration: 0.3 }}
+    >
       <SubLayout pageTitle="비밀번호 변경">
         <ChangePassword />
       </SubLayout>
-    </>
+    </motion.div>
   );
 };
 
