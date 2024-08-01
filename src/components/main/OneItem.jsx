1928d0d815aee9519ce8f64ecea867e7f8056115
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-08-01 11:21:38 +0900

    Fix: 메인 페이지에서 이미지 캐러셀이 동작하지 않는 문제 수정 imagebackground 에 Link 컴포넌트 적용

--- src/components/main/OneItem.jsx
+++ src/components/main/OneItem.jsx
@@ -133,23 +133,23 @@ const OneItem = ({v, startSearch}) => {
     console.log(images.length)
     return (
         <ItemDiv id={v.nanoid}>
-            <Link to={`room/details/${v.nanoid}?${formattedString}`}>
-                    <ItemImagePrev onClick={onClickItemImagePrev}>{"<"}</ItemImagePrev>
-                    <ItemImageNext onClick={onClickItemImageNext}>{">"}</ItemImageNext>
+                <ItemImagePrev onClick={onClickItemImagePrev}>{"<"}</ItemImagePrev>
+                <ItemImageNext onClick={onClickItemImageNext}>{">"}</ItemImageNext>
+                <Link to={`room/details/${v.nanoid}?${formattedString}`}>
                     <ItemBackgroundDiv ref={backgroundRef} $background={images[index]} />
-                        <DotDiv>
-                            {images.map((v, i) => {
-                                return (
-                                    <Dot key={i} $index={i} $imgIndex={index} />
-                                )
-                            })}
-                        </DotDiv>    
-                    <ItemTextDiv>
-                        <ItemTitle>{v.title}<br /></ItemTitle>
-                        <ItemNormalText>호스트: {v.author.photo && v.author.nickname+v.author.photo || v.author.nickname}<br /></ItemNormalText>
-                        <ItemPriceText>{"₩" + Number(v.price).toLocaleString('ko-KR')}</ItemPriceText><ItemNormalText> /인</ItemNormalText>
-                    </ItemTextDiv>
-            </Link>
+                </Link>
+                    <DotDiv>
+                        {images.map((v, i) => {
+                            return (
+                                <Dot key={i} $index={i} $imgIndex={index} />
+                            )
+                        })}
+                    </DotDiv>    
+                <ItemTextDiv>
+                    <ItemTitle>{v.title}<br /></ItemTitle>
+                    <ItemNormalText>호스트: {v.author.photo && v.author.nickname+v.author.photo || v.author.nickname}<br /></ItemNormalText>
+                    <ItemPriceText>{"₩" + Number(v.price).toLocaleString('ko-KR')}</ItemPriceText><ItemNormalText> /인</ItemNormalText>
+                </ItemTextDiv>
         </ItemDiv>
     )
 }
