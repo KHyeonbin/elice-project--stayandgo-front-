cd1caa29486a7005eddc8b1285f23edef56f7711
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-08-02 16:41:34 +0900

    Fix: 나의 숙소 페이지 싹삭이 수정

--- src/pages/MyAccommodationsPage.jsx
+++ src/pages/MyAccommodationsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SubHeader from "../components/layout/SubHeader";
 import SubLayout from "../components/layout/SubLayout";
 import MainFooter from "../components/layout/MainFooter";
 import MyAccommodations from "../components/myAccommodation/MyAccommodations";
@@ -7,15 +8,16 @@ import { motion } from "framer-motion";
 const MyAccommodationsPage = () => {
   return (
     <>
-      <SubLayout pageTitle="나의 숙소">
+    <SubHeader />
       <motion.div
       initial={{ opacity: 0, transform: 'translateX(100%)' }}
       animate={{ opacity: 1, transform: 'translateX(0)' }}
       transition={{ duration: 0.3 }}
     >
+      <SubLayout pageTitle="나의 숙소">
         <MyAccommodations />
-      </motion.div>
       </SubLayout>
+      </motion.div>
       <MainFooter />
     </>
   );
