ea39a47a593d8a7f47e45b6c323f6e5f031d2067
bini <dovesks@naver.com>
2024-08-02 16:50:03 +0900

    싹싹이 수정

--- src/pages/ChatBotPage.jsx
+++ src/pages/ChatBotPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SubLayout from "../components/layout/SubLayout";
+import SubHeader from "../components/layout/SubHeader";
 import MainFooter from "../components/layout/MainFooter";
 import ChatBot from '../components/profile/ChatBot'
 import { motion } from "framer-motion";
@@ -7,6 +8,7 @@ import { motion } from "framer-motion";
 const ChatBotPage = () => {
     return (
       <>
+        <SubHeader />
         <motion.div
           initial={{ opacity: 0, transform: 'translateX(100%)'}}
           animate={{ opacity: 1, transform: 'translateX(0)'}}
--- src/pages/MyAccReservePage.jsx
+++ src/pages/MyAccReservePage.jsx
@@ -1,6 +1,7 @@
 //나의숙소 예약관리 페이지
 import React from "react";
 import SubLayout from "../components/layout/SubLayout";
+import SubHeader from "../components/layout/SubHeader";
 import MainFooter from "../components/layout/MainFooter";
 import { motion } from "framer-motion";
 import MyAccommodationReserve from "../components/myAccReserve/MyAccReserve";
@@ -9,6 +10,7 @@ const MyAccReservePage = () => {
 
   return (
     <>
+    <SubHeader />
     <motion.div
       initial={{ opacity: 0, transform: 'translateX(100%)'}}
       animate={{ opacity: 1, transform: 'translateX(0)'}}
