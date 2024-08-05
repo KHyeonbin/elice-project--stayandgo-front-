971b748b28de867db4f493218e05e846d882dbd0
boeun <supbe0726@naver.com>
2024-08-06 03:54:05 +0900

    Feat: 스크립트 동적으로 불러오는 훅 생성

---
+++ src/api/hooks.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+function useScript(src) {
+    // Keep track of script status ("idle", "loading", "ready", "error")
+    const [status, setStatus] = useState(src ? "loading" : "idle");
+
+    useEffect(() => {
+        if (!src) {
+            setStatus("idle");
+            return;
+        }
+
+        let script = document.querySelector(`script[src="${src}"]`);
+
+        if (!script) {
+            // Create script
+            script = document.createElement("script");
+            script.src = src;
+            script.async = true;
+            script.integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4";
+            script.crossOrigin="anonymous";
+            script.setAttribute("data-status", "loading");
+
+            document.body.appendChild(script);
+
+            const setAttributeFromEvent = (event) => {
+                script.setAttribute(
+                    "data-status",
+                    event.type === "load" ? "ready" : "error"
+                );
+            };
+
+            script.addEventListener("load", setAttributeFromEvent);
+            script.addEventListener("error", setAttributeFromEvent);
+        } else {
+            setStatus(script.getAttribute("data-status"));
+        }
+
+        const setStateFromEvent = (event) => {
+            setStatus(event.type === "load" ? "ready" : "error");
+        };
+
+        script.addEventListener("load", setStateFromEvent);
+        script.addEventListener("error", setStateFromEvent);
+
+        return () => {
+            if (script) {
+                script.removeEventListener("load", setStateFromEvent);
+                script.removeEventListener("error", setStateFromEvent);
+            }
+        };
+    }, [src]);
+
+    return status;
+}
+
+export { useScript };
