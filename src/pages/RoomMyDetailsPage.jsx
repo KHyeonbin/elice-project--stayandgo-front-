8f5d8624ea8f46594106799ee5aede7e9e6c3388
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-08-05 14:35:36 +0900

    Fix: mydetail 페이지에서도 모달 스크롤 적용

--- src/pages/RoomMyDetailsPage.jsx
+++ src/pages/RoomMyDetailsPage.jsx
@@ -9,8 +9,8 @@ const RoomMyDetailsPage = () => {
   const isLogin = useRecoilValue(isLoginSelector);
   return (
     <motion.div
-      initial={{ opacity: 0, transform: 'translateX(100%)' }}
-      animate={{ opacity: 1, transform: 'translateX(0)' }}
+    initial={{ opacity: 0, position: 'relative', left: '100%' }}
+    animate={{ opacity: 1, position: 'relative', left: '0' }}
       transition={{ duration: 0.3 }}
     >
       <SubHeader isLogin={isLogin} />
