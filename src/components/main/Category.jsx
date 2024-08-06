2cc666349276e019af36a70320910b40ddc71e75
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-08-06 23:04:05 +0900

    Fix: 카테고리 부분에서 y축 휠만 동작할 때 y축 휠을 x축 휠동작으로 변환하는 핸들러 추가

--- src/components/main/Category.jsx
+++ src/components/main/Category.jsx
@@ -16,7 +16,7 @@ const Container = styled.div`
     // x축 scroll 작업
     overflow: hidden;
     ::-webkit-scrollbar {
-    display: none; /* 스크롤바 숨기기 */
+        display: none;
     }
 `
 const ItemDiv = styled.div`
@@ -37,7 +37,6 @@ const ItemDiv = styled.div`
     @supports (scroll-snap-type: x mandatory) {
         // 스크롤 시 하나씩 넘기기
         scroll-snap-type: x mandatory;
-        scroll-behavior: smooth;
         /* 끝에서 바운스 되도록 */
         -webkit-overflow-scrolling: touch;
     }
@@ -79,6 +78,29 @@ const Category = ({setCategory, setPage}) => {
     // 태그(카테고리) 변경될 때 setCategory 작업으로 category 상태 변경
     useEffect(() => {
         setCategory(tag);
+        // y 축 휠 동작 시 x 축 스크롤로 변환 핸들러
+        const handleWheel = (e) => {
+            // X축 스크롤로 변환
+            if (e.deltaY !== 0) {
+                e.preventDefault();
+                const scrollAmount = e.deltaY;
+                e.currentTarget.scrollBy({
+                    left: scrollAmount,
+                    behavior: 'smooth'
+                });
+            }
+        };
+        // 휠 동작 시 x 축 스크롤로 변환하기 위한 쿼리 셀렉터
+        const element = document.querySelector('.scrollable-container');
+        if (element) {
+            element.addEventListener('wheel', handleWheel);
+        }
+        return () => {
+            if (element) {
+                element.removeEventListener('wheel', handleWheel);
+            }
+        };
+
     },[tag])
 
     // mainCatetory 디렉토리 이미지 가져오기
@@ -108,7 +130,7 @@ const Category = ({setCategory, setPage}) => {
 
     return (
         <Container>
-            <ItemDiv>
+            <ItemDiv className="scrollable-container">
                 {sortedImages.map((v, i) => (
                     <Item key={i} onClick={() => onClickImage(i)} style={tag === tagArr[i] ? {borderBottom: "2px solid #333"} : {borderBottom: "none"}}>
                         <ItemImg src={v.src} />
